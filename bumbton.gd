d30e10ca4245924d13159ed24f927bec0b56c944
Nico15037 <40710958+Nico15037@users.noreply.github.com>
2021-10-20 22:28:20 -0700

    forest floor,,,,,est + plaug doktor

--- bumbton.gd
+++ bumbton.gd
@@ -4,4 +4,4 @@ func _ready():
 	get_node("Button").connect("pressed", self, "_on_Button_pressed")
 
 func _on_Button_pressed():
-	get_node("Label").text = "SPOOKS HI!!"
+	get_node("Label").text = "SPOOKS HI!!";
