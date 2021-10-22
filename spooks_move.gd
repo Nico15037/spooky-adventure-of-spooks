641da0185de7405676bae1672920e44de1e9e3f4
Nico15037 <40710958+Nico15037@users.noreply.github.com>
2021-10-21 17:32:48 -0700

    speedin, zoomin even

---
+++ spooks_move.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+export var speed = 15;
+export var jump_power = 20;
+export var gravity = 75;
+
+var velocity = Vector3.ZERO;
+
+func _physics_process(delta):
+	var dir = Vector3.ZERO;
+	if Input.is_action_pressed("move_right"):
+		dir.x += 1;
+	if Input.is_action_pressed("move_left"):
+		dir.x -= 1;
+	if Input.is_action_pressed("move_back"):
+		dir.z += 1;
+	if Input.is_action_pressed("move_forward"):
+		dir.z -= 1;
+	
+	if dir != Vector3.ZERO:
+		dir = dir.normalized();
+	
+	velocity.x = dir.x * speed;
+	velocity.z = dir.z * speed;
+	
+	move_and_slide(velocity, Vector3.UP);
