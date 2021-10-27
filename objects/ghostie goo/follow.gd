4db0f8444aebdcbda5b860563c3c6fd102b512f6
Nico15037 <40710958+Nico15037@users.noreply.github.com>
2021-10-26 22:05:07 -0700

    better stalking fuck timers we got INVERSE

--- objects/ghostie goo/follow.gd
+++ objects/ghostie goo/follow.gd
@@ -3,29 +3,49 @@ extends KinematicBody
 var path = [];
 var path_node = 0;
 
-var speed = 10;
+var speed = 3;
 
 onready var player = $"../../../../the spooks space";
 onready var nav = $"../../.."; #SPOOKS STALKERS
 
 func _ready():
-	print_debug(player.name)
+	player.connect("bugle",self,"move_to"); #attach movement signal
+	player.connect("bagule",self,"no"); #attach movement signal
+	path = nav.get_simple_path(global_transform.origin, player.global_transform.origin);
 
 func _physics_process(delta):
-	#print_debug(path_node < path.size(),path_node,path)
+	
 	if (path_node < path.size()):
 		var dir = (path[path_node] - global_transform.origin);
 		
 		if (dir.length() < 1):
 			path_node += 1;
 		else:
+			if (path_node > 1):
+				speed = lerp(speed,1,0.1);
+			else: #ngl these lerps do literally nothing
+				speed = lerp(speed,3,0.1);
 			move_and_slide(dir.normalized() * speed, Vector3.UP);
+			print(path_node,path.size())
 
-func move_to(target_pos):
-	path = nav.get_simple_path(global_transform.origin, target_pos);
-	print_debug(path)
-	path_node = 0;
+func move_to():
+#	path = nav.get_simple_path(global_transform.origin, player.global_transform.origin);
+#	path_node = 0;
+#	$Timer.start(0.1);
+#	$Timer.set_autostart(true);
+#	print("bro")
+	
+	path.append_array(nav.get_simple_path(path[-1], player.global_transform.origin)); #nav.get_simple_path(global_transform.origin, player.global_transform.origin); 
+	if (path.size() > 1):
+		path.invert();
+		path.resize(1);
+		path.invert();
+		path_node = 0;
+	
+
+func no():
+	$Timer.set_autostart(false);
 
 func _on_Timer_timeout():
-	move_to(player.global_transform.origin);
-	print_debug("KK")
+	path = nav.get_simple_path(global_transform.origin, player.global_transform.origin);
+	path_node = 0;
--- objects/spooks/spooks_move.gd
+++ objects/spooks/spooks_move.gd
@@ -4,23 +4,37 @@ export var speed = 3;
 export var jump_power = 20;
 export var gravity = 75;
 
+signal bugle;
+signal bagule;
+
 var velocity = Vector3.ZERO;
 
 func _physics_process(delta):
 	var dir = Vector3.ZERO;
 	if Input.is_action_pressed("move_right"):
-		dir.x += 5;
+		dir.x += 1;
+		emit_signal("bugle");
 	if Input.is_action_pressed("move_left"):
 		dir.x -= 1;
+		emit_signal("bugle");
 	if Input.is_action_pressed("move_back"):
 		dir.z += 1;
+		emit_signal("bugle");
 	if Input.is_action_pressed("move_forward"):
 		dir.z -= 1;
+		emit_signal("bugle");
+#	if Input.is_action_just_pressed("move_forward") || Input.is_action_just_pressed("move_down") || Input.is_action_just_pressed("move_left") || Input.is_action_just_pressed("move_right"):
+#		emit_signal("bugle");
+#
+#	if Input.is_action_just_released("move_forward") || Input.is_action_just_released("move_down") || Input.is_action_just_released("move_left") || Input.is_action_just_released("move_right"):
+#		emit_signal("bagule");
 	
 	if dir != Vector3.ZERO:
 		dir = dir.normalized();
+	
 	
 	velocity.x = dir.x * speed;
 	velocity.z = dir.z * speed;
 	
 	move_and_slide(velocity, Vector3.UP);
+		
